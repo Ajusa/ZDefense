258e9238b329f4a28f40b32504f47c9eef7748dd
spyperson <jacksoncarroll85@yahoo.com>
2015-07-01 10:49:50 -0400

    Update app.js

--- js/app.js
+++ js/app.js
@@ -7,14 +7,33 @@
         Make sure the total file size (not including music) is under 1mb
         Learn from each other
 */
+
+var VERSION;
+var HEIGHT;
+var WIDTH;
+
 function init(){
     var canvas = document.getElementById('myCanvas');
     var ctx = canvas.getContext('2d');
+    canvas.width = WIDTH;
+    canvas.height = HEIGHT;
+    
+    loop();
 //I am so pro
+//#legyt
 }
 
 function loop(){
-
+    //ctx.save() and ctx.restore() are basically how you animate canvases...*
+    ctx.save();
+        //Drawing code; Javascript equivelant of paint() and repaint()
+    ctx.restore();
+    /*  Anything outside of ctx.restore() is for
+        updating variables, anything not related
+        to ctx functions
+    */
 
     setTimeout(loop, 1000/30); //For 30 fps
 }
+
+// *so I have this book about animating in HTML
