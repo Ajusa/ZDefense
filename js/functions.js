1f4eb891e4b9cf64517bd9d376d8faae5fcffd1d
Ajusa Jain <arhamjain@gmail.com>
2015-07-01 13:59:50 -0400

    Added js files to the index.html

--- js/functions.js
+++ js/functions.js
@@ -6,13 +6,6 @@ function isCollide(a, b) {
         (a.x > (b.x + b.width))
     );
 }
-function randomNumber(minNum,maxNum){      
-    var numLow = minNum;
-    var numHigh = maxNum;
-    if ((!isNaN(numLow)) && (!isNaN(numHigh)) && (parseFloat(numLow) <= parseFloat(numHigh)) && (numLow != '') && (numHigh != '')) {
-       var adjustedHigh = (parseFloat(numHigh) - parseFloat(numLow)) + 1;       
-       var numRand = Math.floor(Math.random()*adjustedHigh) + parseFloat(numLow);
- 		return numRand;
-    }      
-    return false;    
-};
+function randomNumber(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
