9a9463c813fc1e5c2e97d70b2b47fc56d02cb12f
Ajusa Jain <arhamjain@gmail.com>
2015-07-01 10:28:48 -0400

    Init the basic canvas, needs to be responsive

--- js/app.js
+++ js/app.js
@@ -7,3 +7,14 @@
         Make sure the total file size (not including music) is under 1mb
         Learn from each other
 */
+function init(){
+    var canvas = document.getElementById('myCanvas');
+    var ctx = canvas.getContext('2d');
+//I am so pro
+}
+
+function loop(){
+
+
+    setTimeout(loop, 1000/30); //For 30 fps
+}
