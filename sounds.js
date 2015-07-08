1e5264db035e018d4dc0416b7fb034ae8f280d02
Ajusa Jain <arhamjain@gmail.com>
2015-07-07 21:24:37 -0400

    Added all of the music

--- sounds.js
+++ sounds.js
@@ -1,7 +1,7 @@
 var currentMusic;
 function loadRandomMusic(){
 	song = randomNumber(1,5);  //Gets a random song number, since they are named things like 1.mp3
-	currentMusic = new Audio("music/"+song+".mp3");
+	currentMusic = new Audio("music/"+"song ("+song+").mp3");
 	currentMusic.play(); //Rock out brah!
 }
 function pauseMusic () {
